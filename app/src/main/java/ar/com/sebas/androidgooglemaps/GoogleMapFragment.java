f8dcdc9a5a74b65adc757b367702ae43198fc9b1
sebas <sebastianoscarlopz@gmail.com>
2017-10-17 15:54:49 -0300

    Saved Work

---
+++ app/src/main/java/ar/com/sebas/androidgooglemaps/GoogleMapFragment.java
@@ -0,0 +1,50 @@
+package ar.com.sebas.androidgooglemaps;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GoogleMapFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GoogleMapFragment extends SupportMapFragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public GoogleMapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment GoogleMapFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GoogleMapFragment newInstance() {
+        GoogleMapFragment fragment = new GoogleMapFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = super.onCreateView(inflater, container, savedInstanceState);
+        return root;
+    }
+
+}
